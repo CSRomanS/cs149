aec8581187082be028f56f9a51b9d8730004151f
Roman Shpilberg <roman.shpilberg@sjsu.edu>
2023-02-19 00:39:15 -0800

    Fixed file opening

--- A1/countnames.c
+++ A1/countnames.c
@@ -6,14 +6,17 @@
  * Creation Date: 2/16/2023
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+
 const int ROWS = 100;
-const int COLS = 30
+const int COLS = 30;
 
 int main(int argc, char *argv[]){
     char names[ROWS][COLS];
     int nameCount[ROWS];
 
-    printf("You have entered: %s", argv[1]);
+    printf("You have entered: %s\n", argv[1]);
 
     FILE *nameFile = fopen(argv[1], "r");
 
@@ -25,21 +28,26 @@ int main(int argc, char *argv[]){
 
     char nameCursor[COLS];
     if(fgets(nameCursor, COLS+1, nameFile)){
-
+        printf(nameCursor);
     }
+
+    return 0;
 }
 
 /**
  *
  * @param list pointer to a 2d char array of names
  * @param name char[] of name to find
- * @return 0 if no name matched, 1 if matched
+ * @return >0 if no name matched, the line that matched otherwise
  */
 int inList(char **list, char name[COLS]){
     for(int i=0; i<ROWS; i++){ // go through names
-        for(int j=0; i<COLS; j++){
-
+        for(int j=0; i<COLS; j++) { // go through specific name
+            if (list[i][j] != name[j]) {
+                break; // goes on to next name
+            }
+            return i; // return the index found
         }
     }
-    return 0;
+    return -1; // no match found
 }
