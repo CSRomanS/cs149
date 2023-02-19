bb0d353fedd568b0b8114a1f032e38b2e1ba9bec
CSRomanS <roman.shpilberg@sjsu.edu>
2023-02-19 04:22:12 -0800

    Format fixed

--- A1/countnames.c
+++ A1/countnames.c
@@ -2,7 +2,7 @@
  * Description: This file counts the number of names in a given text file
  * Author Name: Roman Shpilberg
  * Author Emails: roman.shpilberg@sjsu.edu
- * Last Modified Date: 2/16/2023
+ * Last Modified Date: 2/19/2023
  * Creation Date: 2/16/2023
  */
 
@@ -21,10 +21,7 @@ int main(int argc, char *argv[]){
     int nameCount[ROWS];
     int nameIndex = 0; // index of the next free row
 
-    printf("You have entered: %s\n", argv[1]);
-
     FILE *nameFile = fopen(argv[1], "r");
-
     // file error
     if(nameFile == NULL){
         printf("cannot open file\n");
@@ -37,7 +34,8 @@ int main(int argc, char *argv[]){
         lineCount++;
         if (nameCursor[strlen(nameCursor) - 1] == '\n') nameCursor[strlen(nameCursor) - 1] = '\0';
         if (strlen(nameCursor) == 0){ // filters empty lines
-            fprintf(stderr, "\tWarning - Line %d is empty.\n", lineCount);
+            fprintf(stderr, "Warning - Line %d is empty.\n", lineCount);
+            continue;
         }
 
         //printf(nameCursor);
@@ -55,6 +53,7 @@ int main(int argc, char *argv[]){
         }
     }
     printNameCount(names, nameCount, nameIndex);
+    fclose(nameFile);
     return 0;
 }
 
@@ -71,17 +70,14 @@ int inList(char list[ROWS][COLS], char name[COLS], int maxNames){
             if (list[i][j] != name[j]) {
                 break; // goes on to next name
             }
-            //printf("\tFound Match\n");
             return i; // return the index found
         }
     }
-    //printf("\tNo Match\n");
     return -1; // no match found
 
 }
 
 void printNameCount(char list[ROWS][COLS], int arrCount[ROWS], int maxNames){
-    printf("Printing Table:\n");
     for(int i=0; i<maxNames; i++){
         printf("%s: %d\n", list[i], arrCount[i]);
     }
