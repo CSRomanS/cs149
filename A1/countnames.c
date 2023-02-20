db0013711b1d48ac858296eefc5a53937f8657a8
CSRomanS <roman.shpilberg@sjsu.edu>
2023-02-20 13:29:16 -0800

    Added Comments

--- A1/countnames.c
+++ A1/countnames.c
@@ -2,7 +2,7 @@
  * Description: This file counts the number of names in a given text file
  * Author Name: Roman Shpilberg
  * Author Emails: roman.shpilberg@sjsu.edu
- * Last Modified Date: 2/19/2023
+ * Last Modified Date: 2/20/2023
  * Creation Date: 2/16/2023
  */
 
@@ -10,8 +10,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-const int ROWS = 100;
-const int COLS = 30;
+const int ROWS = 100; // number of names in the array
+const int COLS = 30; // max length of names
 
 int inList(char list[ROWS][COLS], char name[COLS], int maxNames);
 void printNameCount(char list[ROWS][COLS], int arrCount[ROWS], int maxNames);
@@ -29,11 +29,15 @@ int main(int argc, char *argv[]){
     }
 
     char nameCursor[COLS];
-    int lineCount = 0; // keeps a count of the number of lines read
+    int lineCount = 0; // keeps a count of the number of lines read from the file
     while(fgets(nameCursor, COLS+1, nameFile)){
         lineCount++;
+
+        // strips newline characters
         if (nameCursor[strlen(nameCursor) - 1] == '\n') nameCursor[strlen(nameCursor) - 1] = '\0';
-        if (strlen(nameCursor) == 0){ // filters empty lines
+
+        // filters empty lines
+        if (strlen(nameCursor) == 0){
             fprintf(stderr, "Warning - Line %d is empty.\n", lineCount);
             continue;
         }
@@ -52,20 +56,20 @@ int main(int argc, char *argv[]){
             nameCount[index] += 1; // increments the count of the found name
         }
     }
-    printNameCount(names, nameCount, nameIndex);
+    printNameCount(names, nameCount, nameIndex); // print out the count of names
     fclose(nameFile);
     return 0;
 }
 
 /**
- *
+ * Finds if a name exists in the list
  * @param list pointer to a 2d char array of names
  * @param name char[] of name to find
+ * @param maxNames the number of names loaded in the list
  * @return >0 if no name matched, the line that matched otherwise
  */
 int inList(char list[ROWS][COLS], char name[COLS], int maxNames){
     for(int i=0; i<maxNames; i++){ // go through names
-        //printf("Comparing [%s] to [%s]\n", list[i], name);
         for(int j=0; j<COLS; j++) { // go through specific name
             if (list[i][j] != name[j]) {
                 break; // goes on to next name
@@ -77,6 +81,12 @@ int inList(char list[ROWS][COLS], char name[COLS], int maxNames){
 
 }
 
+/**
+ * Prints out the count of names
+ * @param list the list of names to print out
+ * @param arrCount the count of names list
+ * @param maxNames the number of names loaded in the list
+ */
 void printNameCount(char list[ROWS][COLS], int arrCount[ROWS], int maxNames){
     for(int i=0; i<maxNames; i++){
         printf("%s: %d\n", list[i], arrCount[i]);
